f36ae4869aa2722854c8a96f8cb094a4e1de6248
João Luiz <93620657+JoaoLuizDev@users.noreply.github.com>
2023-03-14 09:06:17 -0300

    Create desenvolvimento 2.py

---
+++ Introdução a Linguagem de Programação/cody park/desenvolvimento 2.py
@@ -0,0 +1,27 @@
+"""
+Desenvolva um código que utilize as seguintes características de um veículo:
+- Quantidade de rodas;
+- Peso bruto em quilogramas;
+- Quantidade de pessoas no veículo.
+
+Com essas informações, o programa mostrará qual é a melhor categoria de habilitação para o veículo informado a partir das condições:
+- A: Veículos com duas ou três rodas;
+- B: Veículos com quatro rodas, que acomodam até oito pessoas e seu peso é de até 3500 kg;
+- C: Veículos com quatro rodas ou mais e com peso entre 3500 e 6000 kg;
+- D: Veículos com quatro rodas ou mais e que acomodam mais de oito pessoas; 
+- E: Veículos com quatro rodas ou mais e com mais de 6000 kg.
+"""
+
+rodas = int(input("Qual a quantidade de rodas do veículo? "))
+peso = float(input("Qual o peso bruto em quilogramas? "))
+pessoas = int(input("Qual a quantidade de pessoas no veículo? "))
+if rodas == 2 or rodas == 3:
+  print("Sua habilitação deve ser categoria A.")
+if rodas >= 4 and pessoas <= 8 and peso <= 3500:
+  print("Sua habilitação deve ser categoria B.")
+if rodas >= 4 and 3500 < peso <= 6000:
+  print("Sua habilitação deve ser categoria C.")
+if rodas >= 4 and pessoas > 8:
+  print("Sua habilitação deve ser categoria D.")
+if rodas >= 4 and peso > 6000:
+  print("Sua habilitação deve ser categoria E.")
