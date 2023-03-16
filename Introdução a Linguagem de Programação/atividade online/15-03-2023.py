acf40a82d50e931405a196d48728b6a494d89fff
João Luiz <93620657+JoaoLuizDev@users.noreply.github.com>
2023-03-15 22:28:42 -0300

    Update 15-03-2023.py

--- Introdução a Linguagem de Programação/atividade online/15-03-2023.py
+++ Introdução a Linguagem de Programação/atividade online/15-03-2023.py
@@ -1,3 +1,15 @@
+"""
+Atividade:
+- criar uma função que receba um numero menor ou igual a 100
+- verificar se o numero é menor ou igual a 100:
+- caso o atenda a condição, uma mensagem de parabens e mostrando o numero
+- caso o numero seja maior que 100, mostrar uma mensagem de erro
+
+Extra: 
+testar se a função aceita numeros do tipo float (decimais) para comparar com o inteiro 100. 
+E caso o python não permita comparar int com float. O que pode ser feito para habilitar a comparação entre int e float(decimais)
+"""
+
 #função em que programa só aceita número inteiro, e informará se o número é menor ou igual a 100, ou maior.
 
 def mostrarnumero():
